4b0cb69f34d3b3c2d12d63442797da6217a0bb57
Rodney Lab <codesign@rodneylab.com>
2022-10-24 10:38:30 +0100

    style: 🛁 linting

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -19,7 +19,7 @@
 			href="https://rodneylab.com/"
 			target="_blank"
 			rel="noopener noreferrer">Rodney Lab</a
-		>. Copyright {COPYRIGHT_ENTITY} 2021–2022.
+		>. Copyright {COPYRIGHT_ENTITY} 2020 – 2022.
 	</div>
 	<nav class="footer-icons">
 		<ul>
