b801af7573f81f89d5e906d8ba1e20b7b1b5f80f
Rodney Lab <codesign@rodneylab.com>
2022-10-24 10:38:03 +0100

    fix: 💫 switch to Svelte environment variables

--- src/lib/config/website.js
+++ src/lib/config/website.js
@@ -1,26 +1,43 @@
-const facebookPageName = import.meta.env.VITE_FACEBOOK_PAGE;
+import {
+	PUBLIC_CONTACT_EMAIL,
+	PUBLIC_FACEBOOK_AUTHOR_PAGE,
+	PUBLIC_FACEBOOK_PAGE,
+	PUBLIC_GITHUB_PAGE,
+	PUBLIC_LINKEDIN_PROFILE,
+	PUBLIC_SITE_URL,
+	PUBLIC_TELEGRAM_USERNAME,
+	PUBLIC_TIKTOK_USERNAME,
+	PUBLIC_TWITTER_USERNAME,
+	PUBLIC_TWITTER_USER_ID,
+	PUBLIC_WIRE_USERNAME,
+} from '$env/static/public';
+
+const facebookPageName = PUBLIC_FACEBOOK_PAGE;
+const facebookAuthorPageName = PUBLIC_FACEBOOK_AUTHOR_PAGE;
 
 const website = {
 	author: 'Rodney Johnson',
-	entity: 'Rodney Lab',
 	ogLanguage: 'en_GB',
 	siteLanguage: 'en-GB',
-	siteTitle: 'SvelteKit SEO Demo Site',
-	siteShortTitle: 'SvelteKit SEO', // used as SchemaOrg siteTitleAlt
+	siteTitle: 'SvelteKit Blog Mdx',
+	siteShortTitle: 'SvelteKit Blog',
 	description:
 		'SvelteKit MDsvex Blog Starter - starter code by Rodney Lab to help you get going on your next blog site',
-	siteUrl: import.meta.env.VITE_SITE_URL,
-	contactEmail: import.meta.env.VITE_CONTACT_EMAIL,
-	facebookAuthorPage: `https://www.facebook.com/${import.meta.env.VITE_FACEBOOK_AUTHOR_PAGE}`,
+	siteUrl: PUBLIC_SITE_URL,
+	backgroundColor: '#1b4079',
+	themeColor: '#d62828',
+	contactEmail: PUBLIC_CONTACT_EMAIL,
+	facebookAuthorPage: `https://www.facebook.com/${facebookAuthorPageName}`,
+	facebookAuthorPageName,
 	facebookPage: `https://www.facebook.com/${facebookPageName}`,
 	facebookPageName,
-	githubPage: import.meta.env.VITE_GITHUB_PAGE,
-	linkedinProfile: import.meta.env.VITE_LINKEDIN_PROFILE,
-	telegramUsername: import.meta.env.VITE_TELEGRAM_USERNAME,
-	tiktokUsername: import.meta.env.VITE_TIKTOK_USERNAME,
-	twitterUsername: import.meta.env.VITE_TWITTER_USERNAME,
-	twitterUserId: import.meta.env.VITE_TWITTER_ID,
-	wireUsername: import.meta.env.VITE_WIRE_USERNAME,
+	githubPage: PUBLIC_GITHUB_PAGE,
+	linkedinProfile: PUBLIC_LINKEDIN_PROFILE,
+	telegramUsername: PUBLIC_TELEGRAM_USERNAME,
+	tiktokUsername: PUBLIC_TIKTOK_USERNAME,
+	twitterUsername: PUBLIC_TWITTER_USERNAME,
+	twitterUserId: PUBLIC_TWITTER_USER_ID,
+	wireUsername: PUBLIC_WIRE_USERNAME,
 };
 
 export { website as default };
